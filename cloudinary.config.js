8393c0beb7eb6bafe8b75d8c207a110311f1f224
ivooo369 <ivoivelinov@abv.bg>
2024-07-21 22:53:48 +0300

    Make a change to my env variables

--- cloudinary.config.js
+++ cloudinary.config.js
@@ -1,7 +1,9 @@
-require("dotenv").config({ path: "./backend/.env" });
-import cloudinary from "cloudinary";
+import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.v2.config({
+dotenv.config({ path: "./backend/.env" });
+
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
